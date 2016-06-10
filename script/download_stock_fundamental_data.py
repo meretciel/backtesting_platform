fe4aa80ff847be825c6920806510e7897bed3abd
Ryan <Ruikun@Ruikuns-MBP.pavlov.gateway>
2016-06-09 22:26:44 -0400

    update

--- script/download_stock_fundamental_data.py
+++ script/download_stock_fundamental_data.py
@@ -16,99 +16,204 @@ data_dir = r'/Users/Ruikun/workspace/backtesting_platform_local/data'
 
 # ========= Download Data From Stockpup.com =================
 
-base_url = r'http://www.stockpup.com/data/'
+def download(output_path=None):
+    """
+    This function will download fundamental data of stocks from stockpup.com. The results will be stored in a
+    pd.DataFrame, which is returned by this function
+    """
+    base_url = r'http://www.stockpup.com/data/'
 
-# we first extract the list of stocks available on the website stockpup.com
-# At the time of writing, there is a Download section on the right side of the home page.
-# we will grab the list of stocks there
+    # we first extract the list of stocks available on the website stockpup.com
+    # At the time of writing, there is a Download section on the right side of the home page.
+    # we will grab the list of stocks there
 
-response = urllib2.urlopen(base_url)
+    response = urllib2.urlopen(base_url)
 
-list_of_stock_symbol = []
-list_duplicates      = []
+    list_of_stock_symbol = []
+    list_duplicates      = []
 
-pattern = '\s+title=\"fundamental_data_excel_(\w+)\.csv\">'
+    pattern = '\s+title=\"fundamental_data_excel_(\w+)\.csv\">'
 
-for line in response.readlines():
-    if line.find('csv') != -1:
-        # print(line)
-        res = re.findall(pattern, line)
-        if res and res[0] in list_of_stock_symbol:
-            print('{} already exist. This file will not be download and the existing one will be removed. Please download manually.'.format(res[0]))
-            list_duplicates.append(res[0])
-            continue
+    for line in response.readlines():
+        if line.find('csv') != -1:
+            # print(line)
+            res = re.findall(pattern, line)
+            if res and res[0] in list_of_stock_symbol:
+                print('{} already exist. This file will not be download and the existing one will be removed. Please download manually.'.format(res[0]))
+                list_duplicates.append(res[0])
+                continue
 
-        res and list_of_stock_symbol.append(res[0])
-
-
-# Here we remove the duplicates in the list of symbol
-# For those symbols that have duplicates, we need to create the list manually.
-list_of_stock_symbol = [x for x in list_of_stock_symbol if not x in list_duplicates]
-
-# Now create the list manually for the symbols that have duplicates.
-# The exact filename can be found via "inspect element"
-print(list_duplicates)
-
-# For now we just ignore these stocks. It is not clear what is going on with these stocks. There may be some
-# mislabeling here.
-pass
+            res and list_of_stock_symbol.append(res[0])
 
 
+    # Here we remove the duplicates in the list of symbol
+    # For those symbols that have duplicates, we need to create the list manually.
+    list_of_stock_symbol = [x for x in list_of_stock_symbol if not x in list_duplicates]
+
+    # Now create the list manually for the symbols that have duplicates.
+    # The exact filename can be found via "inspect element"
+    print(list_duplicates)
+
+    # For now we just ignore these stocks. It is not clear what is going on with these stocks. There may be some
+    # mislabeling here.
+    pass
+
+    # download the csv files
+    # the csv files are load into pd.DataFrame. For each csv file, we add a column indicating the symbol of the stock
+    list_df_stock = []
+    list_failure  = []
+
+    suffix = '_quarterly_financial_data.csv'
+
+    for item in list_of_stock_symbol:
+        filename = ''.join([item,suffix])
+        target_url = urlparse.urljoin(base_url, filename)
+        try:
+            df = pd.read_csv(target_url)
+            df['symbol'] = item
+            list_df_stock.append(df)
+        except Exception as e:
+            print("Failed to download file for {}".format(item))
+            print('Error message:', e)
+            list_failure.append(item)
 
 
-# download the csv files
-# the csv files are load into pd.DataFrame. For each csv file, we add a column indicating the symbol of the stock
-list_df_stock = []
-list_failure  = []
+    df_out = pd.concat(list_df_stock, axis=0)
 
-suffix = '_quarterly_financial_data.csv'
-
-for item in list_of_stock_symbol:
-    filename = ''.join([item,suffix])
-    target_url = urlparse.urljoin(base_url, filename)
-    try:
-        df = pd.read_csv(target_url)
-        df['symbol'] = item
-        list_df_stock.append(df)
-    except Exception as e:
-        print("Failed to download file for {}".format(item))
-        print('Error message:', e)
-        list_failure.append(item)
+    if output_path is not None:
+        df_out.to_csv(output_path, index=False)
+    return df_out
 
 
-# print(list_failure)
+
 
 # ========= Data Preparation =============
 
-# concat all the dataframes in the list
-df = pd.concat(list_df_stock, axis=0)
+def prepare_and_save_data(df, index=None, output_path=None):
+    """
+    This function will prepare the download data to fit the platform.
+    :return:
+    """
+    assert index is not None
+    assert output_path is not None
 
-df.to_csv(path.join(data_dir, 'test_fundamental_data.csv'), index=False)
+    df.columns.name = 'fundamental_variable'
+    df = df.set_index(['Quarter end', 'symbol'])
 
-df.columns.name = 'fundamental_variable'
-df = df.set_index(['Quarter end', 'symbol'])
+    df = df.stack()
+    df = df.unstack(level=1)
 
-df = df.stack()
-df = df.unstack(level=1)
+    # In the initial csv files, None is used to represent missing data
+    df[df == 'None'] = np.nan
 
-# In the initial csv files, None is used to represent missing data
-df[df == 'None'] = np.nan
+    df.columns.name = None
 
-df.columns.name = None
+    #df.reset_index().to_csv(path.join(data_dir, 'fundamental_data_of_stocks_stacked.csv'), index=False)
 
-# df.reset_index().to_csv(path.join(data_dir, 'fundamental_data_of_stocks_stacked.csv'), index=False)
+    # split the large dataframe based on the fundamental_variables
+    # now each DataFrame represent one fundamental variable about a bunch of stocks.
+    df = df.reset_index()
 
-# split the large dataframe based on the fundamental_variables
-# now each DataFrame represent one fundamental variable about a bunch of stocks.
 
-df = df.reset_index()
+    # convert Quarter end to datetime object
+    df['Quarter end'] = pd.to_datetime(df['Quarter end'])
+    df = df.sort(['Quarter end'], axis=0)
+    # convert the strings to float
+    df = df.set_index(['Quarter end', 'fundamental_variable']).astype(float).reset_index(level=1)
 
-def f(df):
-    var_name = df.iloc[0]['fundamental_variable']
-    var_name = re.sub('[-\s\&\/]', '_', var_name)
-    df.to_csv(path.join(data_dir, var_name + '.csv'), index=False)
 
-df.groupby(by=['fundamental_variable']).apply(f)
+    def _reindex(df):
+        # This function will be applied to each fundamental variable group.
+        # It will reindex the DataFrame. It will add the target index to the existing index and sort the new index
+        new_index = index.append(df.index).drop_duplicates().values
+        new_index.sort()
 
-# TODO: The dates of stocks data are not aligned.
+        # fill NaN with forward fill method. We should use forward filling because we can only use historical data
+        # not the data from the future.
+        df = df.reindex(new_index).fillna(method='ffill')
 
+        return df
+
+
+    def _to_csv(df):
+        var_name = df.iloc[0]['fundamental_variable']
+        print("{:<30} {}".format("preparing fundamental variable", var_name))
+        var_name = re.sub('[-\s\&\/]', '_', var_name)           # remove special characters
+        var_name = re.sub('_+', '_', var_name)                  # remove repetitive underscores
+        var_name = re.sub('_$', '', var_name)                   # remove the tailing underscore
+        df.set_index(['Quarter end']).reindex(index).to_csv(path.join(output_path, var_name + '.csv'), index=True)
+
+
+
+    # The following step is to reindex the DataFrame. Note that the reindex is done by each fundamental variable.
+    # The reason for this is if we do the reindex without groupby, there will be NaN in the fundamental_variable after
+    # reindex operation, which is problematic for the forward fill operation of the next step
+    df_reindex = df.groupby(by=['fundamental_variable'], as_index=False).apply(_reindex).sort_index()
+    df_reindex.index = df_reindex.index.droplevel(0)
+
+    # save to csv file by each fundamental variable
+    df_out = df_reindex.reset_index()
+    df_out.groupby(by=['fundamental_variable']).apply(_to_csv)
+
+
+
+
+if __name__ == '__main__':
+    df = download(output_path=path.join(data_dir, 'fundamental_data_of_stocks.csv'))
+    # prepare_and_save_data(index=None, output_path=None)
+
+
+
+
+
+
+#
+#
+# df.groupby(by=['fundamental_variable']).apply(f)
+#
+#
+#
+# df = pd.read_csv(path.join(data_dir, 'fundamental_data_of_stocks_stacked.csv'))
+#
+#
+# df_out = df.groupby(by=['fundamental_variable'], as_index=False).ffill()
+#
+#
+# df_out.to_csv(path.join(data_dir, 'fundamental_data_of_stocks_stacked_filled.csv'), index=False)
+#
+#
+#
+# df_ref = pd.read_pickle(path.join(data_dir, 'high_dataframe'))
+#
+# df_out = df_out.set_index(['Quarter end'])
+#
+# df_out = df_out.reindex(df_ref.index)
+#
+#
+# def g(df):
+#     val = df.iloc[0]['fundamental_variable']
+#     df = df.reindex(df_ref.index)
+#     df['fundamental_variable'] = val
+#     return df
+#
+# df_out2 = df_out.groupby(by=['fundamental_variable'], as_index=False).apply(g).sort_index()
+# df_out2.index = df_out2.index.droplevel(0)
+#
+# df_out3 = df_out2.groupby(by=['fundamental_variable'], as_index=False).ffill().reset_index()
+#
+#
+#
+#
+# df_out3.to_csv(path.join(data_dir, 'fundamental_data_of_stocks_stacked_filled_truncated.csv'), index=False)
+#
+#
+#
+# def f(df):
+#     var_name = df.iloc[0]['fundamental_variable']
+#     var_name = re.sub('[-\s\&\/]', '_', var_name)
+#     df.to_csv(path.join(data_dir, var_name + '.csv'), index=False)
+#
+# df_out3.groupby(by=['fundamental_variable']).apply(f)
+#
+#
+#
--- script/download_stock_price_data.py
+++ script/download_stock_price_data.py
@@ -24,40 +24,91 @@ Attention:
 
 
 import pandas as pd
-import numpy as np
 from pandas.io.data import DataReader
-import datetime as dt
-import os
+from os import path
+
 
 
 # set the start date and end date and the list of stocks
+#
+# start_date = pd.datetime(2009,1,1)
+# end_date   = pd.datetime(2013,06,30)
+# stock_list = ['aapl','msft','mmm','ibm','jpm','wmt','yhoo','gps','ge','f']
+#
+#
+# df = DataReader('AAPL', 'yahoo', start_date, end_date)
+#
+# df.to_csv(path.join(r'/Users/Ruikun/workspace/backtesting_platform_local/data', 'test_index.csv'))
 
-print ('initializing...')
-
-start_time = dt.datetime(2009,1,1)
-end_time   = dt.datetime(2013,06,30)
-
-stock_list = ['aapl','msft','mmm','ibm','jpm','wmt','yhoo','gps','ge','f']
-
-
-# create data container
-# this is a dictionary. The key is the ticker of the stock and the value is the
-# dataframe that contains the historical information of that stock
-
-
-print ('downloading historical data from yahoo finance ...')
-data_container = dict()
-
-for stock in stock_list :
-    data_container[stock] = DataReader(stock, 'yahoo', start_time, end_time)
+_default_list_stock = ['aapl','msft','mmm','ibm','jpm','wmt','yhoo','gps','ge','f']
 
 
+def download(start_date=None, end_date=None, list_stock=None, output_path=None):
+    """
+    This function will download data from Yahoo Finance.
 
-# Attention:
-# ^^^^^^^^^
-# need to add some check functionality here.
-# This step is necessary because some of the stocks entered the market recently and for these
-# stocks they do not have enough history
+    Args:
+        start_date:    datetime object indicating the start of the period
+        end_date:      datetime object indicating the end of the period
+        list_of_stock: list of stocks of interest. The stock is identified by symbol.
+        output_path:   path to store the download files.
+
+    Return:
+        The function will return the Date index of the download files. This is useful when we need to reindex other
+        pd.DataFrame.
+
+    """
+    assert start_date
+    assert end_date
+    assert output_path
+
+    list_stock = list_stock or _default_list_stock
+    list_stock = [x.upper() for x in list_stock]
+
+    # create data container
+    # this is a dictionary. The key is the ticker of the stock and the value is the
+    # DateFrame that contains the historical information of that stock
+    data_container = dict()
+
+    for stock in list_stock :
+        try:
+            data_container[stock] = DataReader(stock, 'yahoo', start_date, end_date)
+        except Exception as e:
+            print("Failed to download {} price data from Yahoo Finance.".format(stock))
+            print(e)
+
+    open      = pd.DataFrame()
+    close     = pd.DataFrame()
+    low       = pd.DataFrame()
+    high      = pd.DataFrame()
+    volume    = pd.DataFrame()
+    adjClose  = pd.DataFrame()
+
+
+    for ticker in list_stock:
+        open[ticker]     = data_container[ticker]['Open']
+        close[ticker]    = data_container[ticker]['Close']
+        low[ticker]      = data_container[ticker]['Low']
+        high[ticker]     = data_container[ticker]['High']
+        volume[ticker]   = data_container[ticker]['Volume']
+        adjClose[ticker] = data_container[ticker]['Adj Close']
+
+
+    open.to_csv(path.join(output_path,         'open.csv'))
+    close.to_csv(path.join(output_path,       'close.csv'))
+    low.to_csv(path.join(output_path,           'low.csv'))
+    high.to_csv(path.join(output_path,         'high.csv'))
+    volume.to_csv(path.join(output_path,     'volume.csv'))
+    adjClose.to_csv(path.join(output_path, 'ajdClose.csv'))
+
+
+    rtn = adjClose / adjClose.shift(1) - 1.
+    rtn.to_csv(path.join(output_path, 'rtn.csv'))
+
+    return volume.index
+
+
+
 
 
 
@@ -73,57 +124,42 @@ for stock in stock_list :
 
 # Note that the column name of the dataframe should have the same order as the stock_list
 
-print( 'creating DataFrame...')
+
 
 # create price and volume variables
 
-open      = pd.DataFrame()
-close     = pd.DataFrame()
-low       = pd.DataFrame()
-high      = pd.DataFrame()
-volume    = pd.DataFrame()
-adjClose  = pd.DataFrame()
 
-
-for ticker in stock_list:
-    open[ticker] = data_container[ticker]['Open']
-    close[ticker] = data_container[ticker]['Close']
-    low[ticker] = data_container[ticker]['Low']
-    high[ticker] = data_container[ticker]['High']
-    volume[ticker] = data_container[ticker]['Volume']
-    adjClose[ticker] = data_container[ticker]['Adj Close']
-
-
-
-# create return variables
-nrow , ncol    = adjClose.shape
-stReturn_pre   = adjClose.values[1:] / adjClose.values[:-1] - 1.
-stReturn       = pd.DataFrame(np.zeros_like(adjClose),index=adjClose.index, columns=adjClose.columns )
-stReturn[1:]   = stReturn_pre
-stReturn.ix[0] = np.nan
-
-
-
-
-# save dataframe to disk in a binary form
-
-
-print ('saving DataFrame to disk...')
-
-open.to_pickle     (path=r'../data/open_dataframe')
-close.to_pickle    (path=r'../data/close_dataframe')
-low.to_pickle      (path=r'../data/low_dataframe')
-high.to_pickle     (path=r'../data/high_dataframe')
-volume.to_pickle   (path=r'../data/volume_dataframe')
-adjClose.to_pickle (path=r'../data/adjClose_dataframe')
-stReturn.to_pickle (path=r'../data/stReturn_dataframe')
-
-
-
-
-print ('Done.')
-
-
+#
+#
+# # create return variables
+# nrow , ncol    = adjClose.shape
+# stReturn_pre   = adjClose.values[1:] / adjClose.values[:-1] - 1.
+# stReturn       = pd.DataFrame(np.zeros_like(adjClose),index=adjClose.index, columns=adjClose.columns )
+# stReturn[1:]   = stReturn_pre
+# stReturn.ix[0] = np.nan
+#
+#
+#
+#
+# # save dataframe to disk in a binary form
+#
+#
+# print ('saving DataFrame to disk...')
+#
+# open.to_pickle     (path=r'../data/open_dataframe')
+# close.to_pickle    (path=r'../data/close_dataframe')
+# low.to_pickle      (path=r'../data/low_dataframe')
+# high.to_pickle     (path=r'../data/high_dataframe')
+# volume.to_pickle   (path=r'../data/volume_dataframe')
+# adjClose.to_pickle (path=r'../data/adjClose_dataframe')
+# stReturn.to_pickle (path=r'../data/stReturn_dataframe')
+#
+#
+#
+#
+# print ('Done.')
+#
+#
 
 
 
